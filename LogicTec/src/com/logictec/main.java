fe10afc20901a34af5a1bc2003e606f6792cd931
Abrahamon <ipseabraham@gmail.com>
2014-10-30 22:47:00 -0600

    lista funciona como un array o diccionario en python

--- LogicTec/src/com/logictec/main.java
+++ LogicTec/src/com/logictec/main.java
@@ -11,13 +11,16 @@ public class main {
     public static void main(String[] args) {
        // Circuito circuito = new Circuito();
         Lista<String> a = new Lista<>();
+        a.insertar("4");
         a.insertar("3");
         a.insertar("2");
         a.insertar("1");
         
         Lista<String> array = new Lista<>(a);
-        array.insertarEnIndex("3", "String");
-        //array.getDataIndex("3");
+        
+        
+        array.insertarEnIndex("4", "String");
+        System.out.println(array.getDataIndex("21"));
     }
     
 }
